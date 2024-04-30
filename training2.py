3c70434c53bdc64431c36743b3356e16d89a6423
Lab <Lab@Client113.aastu.edu.et>
2024-04-30 02:12:05 -0700

    Lab 2

--- training2.py
+++ training2.py
@@ -15,3 +15,21 @@ def fall_time(h):
        return the square root of the quotient of double height to gravity to find the time 
        needed for an object to fall to the ground"""
     return math.sqrt((2 * h) / 9.81)
+
+def interval_point(a, b, x):
+    """Given points a and b, compute the difference and multiply it by x and add a 
+       to the result to find the position"""
+    return a + (b - a) * x
+
+def impact_velocity(h):
+    """Given height h, compute the velocity v multiplying 9.81 fall_time(h)"""
+    return (fall_time(h)) * 9.81
+
+def signum(x):
+    """Given a number x, return 1 if x is greater than 0, return 0 if x is 0 return -1 otherwise"""
+    if (x > 0):
+        return 1
+    if (x == 0):
+        return 0
+    else:
+        return -1
